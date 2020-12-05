20daa9f6f1992c525e65e0c35555906d6e8b7c9d
onehao <onewanghao@gmail.com>
2020-12-05 18:33:25 +0800

    change route func

--- Sources/SwiftRouter/SwiftRouter.swift
+++ Sources/SwiftRouter/SwiftRouter.swift
@@ -1,8 +1,16 @@
 import Foundation
 
 public class Router: NSObject {
-    public static func route(_ target: Routeable) {
-        let handler = target.handler
+//    public static func route(_ target: Routeable) {
+//        let handler = target.handler
+//        handler.perform()
+//    }
+    
+    public static func route(_ target: Any?) {
+        guard let t = target as? Routeable else {
+            return
+        }
+        let handler = t.handler
         handler.perform()
     }
 }
