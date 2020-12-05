b009e3af78086018a2043c197d6f5bee7f78621e
onehao <onewanghao@gmail.com>
2020-12-05 17:15:00 +0800

    add one target

---
+++ Sources/SwiftRouter/ModuleOneTarget.swift
@@ -0,0 +1,15 @@
+//
+//  ModuleOneTarget.swift
+//  
+//
+//  Created by wanghao on 2020/12/5.
+//
+
+import Foundation
+//import UIKit
+
+enum ModuleOneTarget {
+    case listPage
+    case detailPage(_ itemId: String)
+//    case colorfulPage(_ color: UIColor)
+}
