05d705dfb1e104430f73f74402cc59937df30ac2
王浩 <wh@addcn.com>
2020-12-07 10:40:42 +0800

    add new page

--- Sources/SwiftRouter/ModuleOneTarget.swift
+++ Sources/SwiftRouter/ModuleOneTarget.swift
@@ -4,12 +4,14 @@
 //
 //  Created by wanghao on 2020/12/5.
 //
-
+#if canImport(UIKit)
 import Foundation
-//import UIKit
+import UIKit
 
 public enum ModuleOneTarget {
     case listPage
     case detailPage(_ itemId: String)
-//    case colorfulPage(_ color: UIColor)
+    case colorfulPage(_ color: UIColor)
 }
+
+#endif
