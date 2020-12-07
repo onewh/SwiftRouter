930a314ed03a1068db2342fb83d026c8696c361f
王浩 <wh@addcn.com>
2020-12-07 15:33:48 +0800

    Module Two

---
+++ ModuleTwoTarget.swift
@@ -0,0 +1,18 @@
+//
+//  ModuleTwoTarget.swift
+//  SwiftRouter
+//
+//  Created by onewh on 2020/12/7.
+//
+
+#if canImport(UIKit)
+import Foundation
+import UIKit
+
+public enum ModuleTwoTarget {
+    case itemList
+    case showImage(_ image: UIImage)
+    case colorfulPage(_ color: UIColor)
+}
+
+#endif
